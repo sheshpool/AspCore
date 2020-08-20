39e955b6714af4949b16b17a506ed0788ec53b96
sheshpool <sheshpool@protonmail.com>
2020-08-20 10:47:21 +0200

    Create solution

---
+++ Web.UI/Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Aventure";
+            ViewBag.Tableau = new string[] { "Aventure 1", "Aventure 2", "Aventure 3", "Aventure 4", "Aventure 5" };
+            return View();
+        }
+    }
+}
