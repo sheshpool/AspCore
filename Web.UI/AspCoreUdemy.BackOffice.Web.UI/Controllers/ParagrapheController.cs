6283c56fef77e79e218f2f06882062d11f2b0351
sheshpool <sheshpool@protonmail.com>
2020-08-20 18:18:46 +0200

    Update

---
+++ Web.UI/AspCoreUdemy.BackOffice.Web.UI/Controllers/ParagrapheController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspCoreUdemy.Core.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreUdemy.BackOffice.Web.UI.Controllers
+{
+    public class ParagrapheController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Paragraphe paragraphe = new Paragraphe() { Id = id, Titre = "Titre du paragraphe", Description = "Description du paragraphe", Numero = 10 };
+            return View(paragraphe);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Paragraphe paragraphe)
+        {
+
+            return RedirectToAction("Create");
+        }
+    }
+}
