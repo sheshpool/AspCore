9a0e3d82a9026393b3fd6c8bdb7e05682cabf639
sheshpool <sheshpool@protonmail.com>
2020-08-23 02:42:35 +0200

    Updated

---
+++ AspCoreUdemy/AspProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspCoreUdemy/IdentityOffice365/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityOffice365.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
