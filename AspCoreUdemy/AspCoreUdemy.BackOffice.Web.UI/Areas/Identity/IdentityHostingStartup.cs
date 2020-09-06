084b7486ffd435a7f774271eb34c471c0b257811
sheshpool <sheshpool@protonmail.com>
2020-09-06 02:52:29 +0200

    Update

---
+++ AspCoreUdemy/AspCoreUdemy.BackOffice.Web.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AspCoreUdemy.BackOffice.Web.UI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspCoreUdemy.BackOffice.Web.UI.Areas.Identity.IdentityHostingStartup))]
+namespace AspCoreUdemy.BackOffice.Web.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AspCoreUdemyBackOfficeWebUIDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AspCoreUdemyBackOfficeWebUIDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AspCoreUdemyBackOfficeWebUIDbContext>();
+            });
+        }
+    }
+}
---
+++ AspCoreUdemy/AspCoreUdemy.Core.Data/Models/ExamSession.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCoreUdemy.Core.Data.Models
+{
+    public class ExamSession
+    {
+
+    }
+}
